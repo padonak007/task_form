05bc0ba6733d41703a8746024fa258843a6c1fc3
illia <itchernikow@gmail.com>
2017-06-02 19:55:58 +0300

    Almost implemented last task

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var city,
     $email = $("#email"),
     $country = $("#country"),
     $city = $("#city"),
+    $add = $("[data-add]"),
     re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 var countries = {
@@ -51,7 +52,16 @@ $email.change(function () {
 
 $country.change(function (event) {
     $("[data-not-selected-option]").remove();
+    $(event.target).attr("data-is-not-empty",true);
     fillCitiesDropdown(event.target.value);
+});
+
+$add.click(function (event) {
+    if ($(event.target).prev().attr("data-is-not-empty")) {
+        addInputandButton(event);
+        $(event.target).remove();
+        
+    };
 });
 
 function validateEmail(fieldValue,$field) {
@@ -94,4 +104,22 @@ function fillCitiesDropdown(country) {
         .text(cities[country][key])
         .appendTo($city);
     };
+}
+
+function addInputandButton(event) {
+    $("<input/>")
+        .attr("type", "text")
+        .appendTo($(event.target).parent())
+        .change(function (event) {
+            $(event.target).next().attr("disabled", false);
+        });
+    $("<button>")
+        .text("+")
+        .attr("disabled",true)
+        .appendTo($(event.target).parent())
+        .click(function (event) {
+            $("<option/>")
+            .text($(event.target).prev("input").val())
+            .appendTo($(event.target).siblings("select"));
+        });
 }
